c11dd19b3b6a6cb7c1b4b76a9818bed324d087e9
Hanschristianhennoen <hanschr91@hotmail.com>
2014-09-01 21:15:02 +0200

    Send login data to server

---
+++ js/login.js
@@ -0,0 +1,14 @@
+function recieveCallback (data) {
+	if (data.response == 1) {
+		alert("Login");
+	}
+	else {
+		alert("Login fail");
+	}
+};
+
+function login(){
+	var username = $("#username-input-field").val();
+	var password = $("#password-input-field").val();
+	send("register.php", "{username: " + username + ", password: " + password + ", recieveCallback);
+};
