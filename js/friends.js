bda0f57fc22477475a57293d40d1d2f831a2aff4
Stian Weie <stianwe@gmail.com>
2014-09-08 22:04:17 +0200

    Display confirmation/error when adding friend

--- js/friends.js
+++ js/friends.js
@@ -8,6 +8,34 @@ function loadFriends() {
 
 function addFriendCallback(data) {
 	console.log("Response:", data.response);
+	switch (parseInt(data.response)) {
+		case -1:
+			alert("Friend request sent.");
+			break;
+		case -2:
+			alert("You do not appear logged in.")
+			break;
+		case -3:
+			alert("User not found.");
+			break;
+		case -4:
+			alert("SQL error.");
+			break;
+		case -5:
+			alert("You cannot add yourself as a friend.");
+			break;
+		case 0:
+			alert("Friend request already exists.");
+			break;
+		case 1:
+			alert("You are already friends.");
+			break;
+		case 2:
+			alert("Friend request already exists(2).");
+			break;
+		default:
+			alert("Unexpected response from server. Please report this incident. Error code: " + data.response);
+	};
 };
 
 function addFriend() {
