72f007ad926c136c77c452917b345071d69437e8
Stian Weie <stianwe@gmail.com>
2014-09-19 00:10:32 +0200

    Initial debt responding server code

--- php/debt.php
+++ php/debt.php
@@ -3,6 +3,16 @@
 	include "utils.php";
 	include "sql.php";
 
+	define("SQL_ERROR", -3);
+	define("NOT_PERMITTED", -2);
+	define("DEBT_NOT_FOUND", -1);
+	define("REQUESTED", 0);
+	define("ACCEPTED", 1);
+	define("DECLINED", 2);
+	define("COMPLETED_BY_REQUESTING_USER", 3);
+	define("COMPLETED_BY_OTHER_USER", 4);
+	define("COMPLETED", 5);
+
 	function loadDebts($userId) {
 		$mysqli = getMysqlInstance();
 		$query = "SELECT d.id, d.amount, d.what, d.comment, d.fromUser, d.toUser, d.requestingUser, d.status, u.username
@@ -19,6 +29,53 @@
 		}
 
 		return $debts;
+	}
+
+	function changeStatusOfDebt($debtId, $newStatus, $userId) {
+		$mysql = getMysqlInstance();
+		$response = 0;
+		// First check that the user can change the status of the debt (and that the debt exists)
+		$query = "SELECT status, fromUser, toUser, requestingUser FROM debt where id=\"{$debtId}\"";
+		if ($res = $mysql->query($query)) {
+			if ($row = $res->fetch_assoc()) {
+				$oldStatus = $row["status"];
+				$fromUser = $row["fromUser"];
+				$toUser = $row["toUser"];
+				$requestingUser = $row["requestingUser"];
+				switch ($newStatus) {
+					case ACCEPTED:
+						// TODO: MERGE DEBTS
+					case DECLINED:
+						if (!($requestingUser != $userId && ($fromUser == $userId || $toUser == $userId))) {
+							$response = NOT_PERMITTED;
+						}
+						break;
+					case COMPLETED_BY_REQUESTING_USER:
+						if ($requestingUser != $userId) {
+							$response = NOT_PERMITTED;
+						}
+						// We need to check if other user has completed it
+						else if ($oldStatus == COMPLETED_BY_OTHER_USER) {
+							$status = COMPLETED;
+						}
+						break;
+					case COMPLETED_BY_OTHER_USER:
+						if (!($requestingUser != $userId && ($fromUser == $userId || $toUser == $userId))) {
+							$response = NOT_PERMITTED;
+						}
+						// We need to check if requesting user has completed it
+						else if ($status == $COMPLETED_BY_REQUESTING_USER) {
+							$status = COMPLETED;
+						}
+				}
+			} else {
+				$response = DEBT_NOT_FOUND;
+			}
+		} else {
+			$response = SQL_ERROR;
+		}
+		$mysql->close();
+		return $response;
 	}
 
 ?>
---
+++ php/debt_response.php
@@ -0,0 +1,15 @@
+<?php
+
+	include "debt.php";
+
+	$rcvd = receive();
+	$data = json_decode($rcvd[1]);
+	$userId = verifyUserHelper($data);
+	$response = -1;
+	if ($userId != -1) {
+		// Valid user credentials
+		// TODO 
+	}
+	send($rcvd[0], json_encode(array("response" => $response)));
+
+?>
