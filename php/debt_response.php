d04740267f6403eb40dae54e0bf044c207972d96
Stian Weie <stianwe@gmail.com>
2014-10-01 15:07:32 +0200

    Remove SQL injection vulnerability

--- php/debt_response.php
+++ php/debt_response.php
@@ -8,7 +8,7 @@
 	$response = INVALID_USER_CREDENTIALS;
 	if ($userId != -1) {
 		// Valid user credentials
-		$response = changeStatusOfDebt($data->{"debtId"}, $data->{"newStatus"}, $userId);
+		$response = changeStatusOfDebt(intval($data->{"debtId"}), intval($data->{"newStatus"}), $userId);
 	}
 	send($rcvd[0], json_encode(array("response" => $response)));
 
