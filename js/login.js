697ad113f7276a55cd73baded66079144361bc52
Stian Weie <stianwe@gmail.com>
2014-10-01 16:48:05 +0200

    Persistent login - close #4

--- js/login.js
+++ js/login.js
@@ -1,6 +1,12 @@
+var USERNAME_COOKIE_NAME = "debtlist2_username";
+var PASSWORD_COOKIE_NAME = "debtlist2_password";
+
 function loginRecieveCallback (data) {
 	if (data.id != -1) {
 		console.log("Logged in with id", data.id);
+		// Set cookies to allow auto-login if login was successful
+		$.cookie(USERNAME_COOKIE_NAME, window.username);
+		$.cookie(PASSWORD_COOKIE_NAME, window.password);
 		enterPostLogin(data.id);
 	}
 	else {
@@ -8,8 +14,21 @@ function loginRecieveCallback (data) {
 	}
 };
 
-function login(){
-	window.username = $("#username-input-field").val();
-	window.password = $("#password-input-field").val();
+function login(auto) {
+	if (!auto) {
+		window.username = $("#username-input-field").val();
+		window.password = $("#password-input-field").val();
+		console.log("Manual login");
+	} else {
+		console.log("Auto-login");
+	}
 	sendWithCredentials("login.php", "{}", loginRecieveCallback);
+};
+
+function tryAutoLogin() {
+	if ($.cookie(USERNAME_COOKIE_NAME) != undefined && $.cookie(PASSWORD_COOKIE_NAME) != undefined) {
+		window.username = $.cookie(USERNAME_COOKIE_NAME);
+		window.password = $.cookie(PASSWORD_COOKIE_NAME);
+		login(true);
+	}
 };
